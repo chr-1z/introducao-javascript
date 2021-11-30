c9ab7dd7ef2aaf5b206396fe6e6af8dab1f8fd2d
Christian Oliveira Leichtweis <chris99leichtweiss@gmail.com>
2021-11-30 19:33:42 -0300

    Aula 3

--- teste/teste.js
+++ teste/teste.js
@@ -4,10 +4,25 @@
 
 // var total = preco - desconto;
 
-function soma(a, b) {
-    return a+b;
+
+// function soma(a, b) {
+//     return a+b;
+// }
+
+// soma(5+3);
+
+//console.log("Hello World!!");
+
+function returnEvenValues(array){
+    let evenNums = [];
+    for(let i=0; i<array.length; i++){
+        if(array[i] % 2 === 1){
+            evenNums.push(array[i]);
+        }
+    }
+    console.log("Os números ímpares são: ", evenNums);
 }
 
-soma(5+3);
+let array = [1, 2, 4, 5, 7, 8];
 
-
+returnEvenValues(array);
