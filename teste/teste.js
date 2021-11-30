030533b941de810c70bf723a77410d54273333b9
Christian Oliveira Leichtweis <chris99leichtweiss@gmail.com>
2021-11-30 19:07:23 -0300

    Aula 2

--- teste/teste.js
+++ teste/teste.js
@@ -1,8 +1,13 @@
 // var preco = 2
-var desconto = 0.2;
-const preco = 2;
+// var desconto = 0.2;
+// const preco = 2;
 
-var total = preco - desconto;
+// var total = preco - desconto;
 
+function soma(a, b) {
+    return a+b;
+}
+
+soma(5+3);
 
 
